8d6ef7f481807bdb786553adf3384e4b92a87608
Jadiê <jadieoliveira00@gmail.com>
2020-08-04 21:54:57 -0300

    _app e styled global

---
+++ src/styles/globals.ts
@@ -0,0 +1,18 @@
+import {createGlobalStyle} from 'styled-components'
+
+const GlobalStyleComponent = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: 0;
+  }
+  html {
+    font-size: 62.5%
+  }
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+  }
+
+`
+
+export default GlobalStyleComponent
