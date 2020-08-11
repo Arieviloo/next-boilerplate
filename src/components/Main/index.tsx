67498248240c6589e6c5aed87e6a72fd2bcd3058
Jadiê <jadieoliveira00@gmail.com>
2020-08-11 11:13:33 -0300

    styled

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,20 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e React avançado ao lado"
+    />
+    <S.Title>React Avançado</S.Title>
+    <S.Description>
+      TypeScript, ReactJS, NextJS, Styled Components
+    </S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="desenvolvedor em frente ao um computador"
+    />
+  </S.Wrapper>
 )
 
 export default Main
---
+++ src/components/Main/styles.ts
@@ -0,0 +1,31 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.main`
+  background-color: #06092b;
+  color: #fff;
+  width: 100%;
+  height: 100vh;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+export const Logo = styled.img`
+  width: 25rem;
+`
+
+export const Title = styled.h1`
+  font-size: 2.5rem;
+`
+
+export const Description = styled.h2`
+  font-size: 2rem;
+  font-weight: 400;
+`
+
+export const Illustration = styled.img`
+  margin-top: 2rem;
+  width: min(30%, 100%);
+`
